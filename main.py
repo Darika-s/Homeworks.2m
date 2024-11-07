de1ee93cd9f7b812e60e78ed0bfb9f2a9987a50b
Darika-s <soburbekova@gmail.com>
2024-11-07 15:47:28 +0600

    Feat: hw2 My Hero

---
+++ main.py
@@ -0,0 +1,14 @@
+class Hero:
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+    def action(self):
+        print(f'{self.name} выполняет действие.')
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            print(f'{self.name} был побежден.')
+        else:
+            print(f'{self.name} получил {damage} урона, здоровье теперь {self.health}.')
